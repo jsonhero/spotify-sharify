3170c4dba4621dd949c40a2b54a6f2e83e2e77a1
Jason Bratt <jasonhero858@gmail.com>
2018-04-10 22:56:00 -0500

    auth flow

--- init.py
+++ init.py
@@ -1,5 +1,44 @@
-from flask import Flask
+import os
+import requests
+import base64
+from urllib.parse import urlencode
+from flask import Flask, request, redirect, jsonify
 
 app = Flask(__name__)
+
+redirect_uri = "http://localhost:3000/callback"
+clientId = os.environ.get("clientId")
+clientSecret = os.environ.get("clientSecret")
+
+@app.route('/spotify')
+def authenticate():
+  queryStrings = urlencode({
+    "client_id": clientId,
+    "response_type": "code",
+    "redirect_uri": redirect_uri,
+  })
+  return redirect("https://accounts.spotify.com/authorize?" + queryStrings)
+
+@app.route('/callback')
+def callback():
+  code = request.args.get('code')
+  state = request.args.get('state')
+  payload = {
+    "grant_type": "authorization_code",
+    "code": code,
+    "redirect_uri": redirect_uri,
+  }
+  basic = "{id}:{secret}".format(id=clientId, secret=clientSecret)
+  headers = {
+    "Authorization": "Basic " + base64.b64encode(bytes(basic, 'utf-8')).decode()
+  }
+  response = requests.post("https://accounts.spotify.com/api/token", data=payload, headers=headers)
+  json = response.json()
+  return jsonify(json)
+
+
+
+
+
 
 app.run(port=3000)
